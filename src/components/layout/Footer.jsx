f2c91bea1671b30dbc0988fbf0e894222bd379e4
vijitdua <82555472+vijitdua@users.noreply.github.com>
2024-07-13 22:13:07 +0900

    Feat: Footer mobile display, Refactoring
    - Footer.jsx would not respond to mobile view, and would create two columns. Now creates two rows instead of mobile view.

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,47 +1,50 @@
 // Footer.js
 import React from 'react';
-import { Box, Link, Typography } from '@mui/material';
+import {Box, Link, Typography, useMediaQuery} from '@mui/material';
 import {socialIconsList} from "../../configs/menuConfig";
 import SocialIcon from "../SocialIcon";
 import {contactRoutes, routes} from "../../configs/routesConfig";
+import {theme} from "../../themes/primaryTheme";
 
-const Footer = () => {
-  return (
-    <footer
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        padding: '1rem 0',
-        backgroundColor: '#333',
-        color: '#fff',
-      }}
-    >
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'space-evenly',
-          width: '100%',
-          maxWidth: '100%',
-          padding: '0 2rem',
-          textAlign: 'center',
-        }}
-      >
-        <Typography variant="body2">
-          &copy; {new Date().getFullYear()} Icarus Development. All rights reserved.
-        </Typography>
+function Footer() {
 
-        <Box sx={{ display: 'flex', alignItems: 'center' }}>
-          <Typography variant="body2" sx={{ marginRight: '2rem' }}>
-            {contactRoutes.email.split(':')[1]}
-          </Typography>
-            {socialIconsList.map((item) => SocialIcon(item))}
-        </Box>
+    const mobileView = useMediaQuery(theme.breakpoints.down('md'));
 
-      </Box>
-    </footer>
-  );
-};
+    return (
+        <footer
+            style={{
+                display: 'flex',
+                justifyContent: 'center',
+                padding: '1rem 0',
+                backgroundColor: '#333',
+                color: '#fff',
+            }}
+        >
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: mobileView? 'column' : 'row',
+                    alignItems: 'center',
+                    justifyContent: mobileView ? 'flex-start' : 'space-evenly',
+                    width: '100vw',
+                    maxWidth: '100vw',
+                    textAlign: 'center',
+                }}
+            >
+                <Typography variant="body2">
+                    &copy; {new Date().getFullYear()} Icarus Development. All rights reserved.
+                </Typography>
+
+                <Box sx={{display: 'flex', alignItems: 'center'}}>
+                    <Typography variant="body2" sx={{marginRight: '2rem'}}>
+                        {contactRoutes.email.split(':')[1]}
+                    </Typography>
+                    {socialIconsList.map((item) => SocialIcon(item))}
+                </Box>
+
+            </Box>
+        </footer>
+    );
+}
 
 export default Footer;
