b362447795f23ada930122a6dedcf4e844bf6074
vijitdua <82555472+vijitdua@users.noreply.github.com>
2024-07-22 00:39:07 +0900

    feat: Dockerfile + ready for production (dummy data)

--- src/services/teamDataService.js
+++ src/services/teamDataService.js
@@ -15,79 +15,32 @@ export async function getTeamMemberData(){
     return [
 
         {
-            name: 'Team Member 1',
-            role: 'developer',
-            specificRole: 'Lead Developer of Team 1',
-            // Random image of a pumpkin for now, this is a good test to ensure it is wrapped in a MUI avatar and ensure and it is rendered with a proper width and height removing extra background!
-            profileImage: 'https://preview.redd.it/i-got-bored-so-i-decided-to-draw-a-random-image-on-the-v0-4ig97vv85vjb1.png?width=640&crop=smart&auto=webp&s=22ed6cc79cba3013b84967f32726d087e539b699',
-            profileLink: 'https://just-an-example-url.icarusdevteam.com', // This can be linked in or any other link we choose
-            bio: 'About the team member',
-        },
-
-        {
-            name: 'Team Member 2',
-            role: 'designer',
-            // Random image for now, this is a good test to ensure it is wrapped in a MUI avatar and ensure and it is rendered with a proper width and height removing extra background!
-            profileImage: 'https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg',
-            profileLink: 'https://just-an-example-url-two.icarusdevteam.com', // This can be linked in or any other link we choose
-            bio: 'About the team member 2',
+            name: 'Coming soon',
+            specificRole: '',
+            profileImage: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+            profileLink: '/', // This can be linked in or any other link we choose
+            bio: 'Details of the members of our team are coming soon!',
         },
         {
-            name: 'Team Member 2',
-            role: 'designer',
-            // Random image for now, this is a good test to ensure it is wrapped in a MUI avatar and ensure and it is rendered with a proper width and height removing extra background!
-            profileImage: 'https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg',
-            profileLink: 'https://just-an-example-url-two.icarusdevteam.com', // This can be linked in or any other link we choose
-            bio: 'About the team member 2',
+            name: 'Coming soon',
+            specificRole: '',
+            profileImage: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+            profileLink: '/', // This can be linked in or any other link we choose
+            bio: 'Details of the members of our team are coming soon!',
         },
         {
-            name: 'Team Member 2',
-            role: 'designer',
-            // Random image for now, this is a good test to ensure it is wrapped in a MUI avatar and ensure and it is rendered with a proper width and height removing extra background!
-            profileImage: 'https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg',
-            profileLink: 'https://just-an-example-url-two.icarusdevteam.com', // This can be linked in or any other link we choose
-            bio: 'About the team member 2',
+            name: 'Coming soon',
+            specificRole: '',
+            profileImage: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+            profileLink: '/', // This can be linked in or any other link we choose
+            bio: 'Details of the members of our team are coming soon!',
         },
         {
-            name: 'Team Member 2',
-            role: 'designer',
-            // Random image for now, this is a good test to ensure it is wrapped in a MUI avatar and ensure and it is rendered with a proper width and height removing extra background!
-            profileImage: 'https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg',
-            profileLink: 'https://just-an-example-url-two.icarusdevteam.com', // This can be linked in or any other link we choose
-            bio: 'About the team member 2',
+            name: 'Coming soon',
+            specificRole: '',
+            profileImage: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+            profileLink: '/', // This can be linked in or any other link we choose
+            bio: 'Details of the members of our team are coming soon!',
         },
-        {
-            name: 'Team Member 2',
-            role: 'designer',
-            // Random image for now, this is a good test to ensure it is wrapped in a MUI avatar and ensure and it is rendered with a proper width and height removing extra background!
-            profileImage: 'https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg',
-            profileLink: 'https://just-an-example-url-two.icarusdevteam.com', // This can be linked in or any other link we choose
-            bio: 'About the team member 2',
-        },
-        {
-            name: 'Team Member 2',
-            role: 'designer',
-            // Random image for now, this is a good test to ensure it is wrapped in a MUI avatar and ensure and it is rendered with a proper width and height removing extra background!
-            profileImage: 'https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg',
-            profileLink: 'https://just-an-example-url-two.icarusdevteam.com', // This can be linked in or any other link we choose
-            bio: 'About the team member 2',
-        },
-        {
-            name: 'Team Member 2',
-            role: 'designer',
-            // Random image for now, this is a good test to ensure it is wrapped in a MUI avatar and ensure and it is rendered with a proper width and height removing extra background!
-            profileImage: 'https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg',
-            profileLink: 'https://just-an-example-url-two.icarusdevteam.com', // This can be linked in or any other link we choose
-            bio: 'About the team member 2',
-        },
-        {
-            name: 'Team Member 2',
-            role: 'designer',
-            // Random image for now, this is a good test to ensure it is wrapped in a MUI avatar and ensure and it is rendered with a proper width and height removing extra background!
-            profileImage: 'https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg',
-            profileLink: 'https://just-an-example-url-two.icarusdevteam.com', // This can be linked in or any other link we choose
-            bio: 'About the team member 2',
-        },
-
     ];
 }
