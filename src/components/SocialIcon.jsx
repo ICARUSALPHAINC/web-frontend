27962130e284aeaae2d2fe6803a92c5ef2d68fa8
jadenzy <jadenyang65809@gmail.com>
2024-07-21 23:56:42 -0700

    fix: contact page, 'apply' in new page, email button function}, make footer more smilar as header

--- src/components/SocialIcon.jsx
+++ src/components/SocialIcon.jsx
@@ -3,14 +3,20 @@ import {Link} from "@mui/material";
 
 function SocialIcon(item) {
     return (
-        <Link href={item.link} target="_blank" sx={{
-            margin: '0 0.5rem',
-            fontSize: 40,
-            color: '#fff',
-            transition: 'scale 0.5s',
-            '&:hover': {scale: '1.2'}
-        }}>
-            {item.icon}
+        <Link
+            component={item.route ? 'a' : 'button'} 
+            href={item.route ? item.route : undefined} 
+            target={item.route ? "_blank" : undefined} 
+            rel={item.route ? "noopener noreferrer" : undefined}
+            onClick={(item.onClickFunction && !item.route) ? item.onClickFunction : undefined}
+            sx={{
+                margin: '0 0.5rem',
+                fontSize: 40,
+                color: '#fff',
+                transition: 'scale 0.5s',
+                '&:hover': {scale: '1.2'}
+            }}>
+                {item.icon}
         </Link>);
 }
 
