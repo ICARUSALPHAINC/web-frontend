e7ddad57ce589a48adcf1fa9413546db6594e4cf
kwon-23 <bhkwon@ucsd.edu>
2025-06-26 21:40:50 -0700

    Added project images, removed AI mentions
    Added rudimentary images for projects (did Aescension for Indie game picture since there were none for Indie games) and removed mention of AI on homepage

--- src/screens/HomePage.jsx
+++ src/screens/HomePage.jsx
@@ -277,7 +277,7 @@ function HomePage() {
                             <Typography variant="body1" sx={{
                                 flex: 1,
                             }}>
-                                Our projects span various industries, from AI and gaming to social media and
+                                Our projects span various industries, from gaming to social media and
                                 productivity tools. At Icarus Development, we tackle complex challenges and develop
                                 forward-thinking solutions that make a meaningful impact. We are committed to pushing
                                 technological boundaries and creating groundbreaking advancements.
