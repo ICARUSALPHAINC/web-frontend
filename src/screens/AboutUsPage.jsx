bbff9cc2ba29b0ee993814d1dec8debb08f1047b
CHANGUSCHONGUS\steve <steven.chang878@gmail.com>
2026-02-26 22:39:36 -0800

    img changed from bg to box, tweak mobile view

--- src/screens/AboutUsPage.jsx
+++ src/screens/AboutUsPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Typography} from "@mui/material";
+import { Box, Typography } from "@mui/material";
 function AboutUsPage() {
 
   return (
@@ -18,59 +18,64 @@ function AboutUsPage() {
     >
       <Box
         sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          backgroundImage: "url(/about-images/about-us-block.webp)",
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "contain",
-          backgroundPosition: "center",
-          height: "70%",
-          width: "70%",
-          padding: "2rem",
-          margin: "2rem",
-          border: '2px solid red',
-          borderRadius: '10px',
+          position: "relative",
+          display: "inline-block",
         }}
       >
-        <Typography
-          variant="h5"
+        <Box
+          component="img"
+          src="/about-images/about-us-block.webp"
           sx={{
+            display: "block",
+            width: "min(800px, 80vw)",
+            height: "auto",
+            border: "2px solid green",
+            borderRadius: "10px",
+          }}
+        />
+
+        <Box
+          sx={{
+            position: "absolute",
+            inset: 0,
             display: "flex",
-            flexDirection: "column",
             justifyContent: "center",
             alignItems: "center",
-            height: "60%",
-            maxHeight: "100%",
-            width: "50%",
-            // padding: '2rem',
-            margin: "2rem",
-            fontSize: "clamp(8px, 1vw, 24px)",
-            // overflow: 'hidden',
-            textAlign: "center",
-            wordBreak: "break-word",
-            border: '2px solid red',
-            borderRadius: '10px',
+            border: "2px solid blue",
+            borderRadius: "10px",
           }}
         >
-          Redefining the Future of Play
-          <br/>
-          <br/>
-          Icarus Alpha is a next-generation game studio dedicated to building groundbreaking digital experiences.
-          We unite a world-class team of developers, artists, and visionaries under one mission: to redefine what’s possible for interactive entertainment.
-          <br/>
-          <br/>
-          Our studio focuses on delivering projects that combine creativity and technical innovation at the highest level.
-          By pushing the limits of storytelling and world-building, we aim to create unforgettable experiences that elevate the future of play.
-          <br/>
-          <br/>
-          At Icarus Alpha, our greatest strength is our team – creators, innovators, and leaders who challenge each other to achieve more.
-          We are building something big, and the opportunity is open to all who share our vision.
-          <br/>
-          <br/>
-          Together, we are shaping ideas into reality.
+          <Typography
+            variant="h5"
+            sx={{
+              fontSize: "clamp(1vw, 12px, 2vw)",
+              textAlign: "center",
+              wordBreak: "break-word",
+              maxHeight: "80%",
+              maxWidth: "80%",
+              border: "2px solid red",
+              borderRadius: "10px",
+            }}
+          >
+            Redefining the Future of Play
+            <br />
+            <br />
+            Icarus Alpha is a next-generation game studio dedicated to building groundbreaking digital experiences.
+            We unite a world-class team of developers, artists, and visionaries under one mission: to redefine what’s possible for interactive entertainment.
+            <br />
+            <br />
+            Our studio focuses on delivering projects that combine creativity and technical innovation at the highest level.
+            By pushing the limits of storytelling and world-building, we aim to create unforgettable experiences that elevate the future of play.
+            <br />
+            <br />
+            At Icarus Alpha, our greatest strength is our team – creators, innovators, and leaders who challenge each other to achieve more.
+            We are building something big, and the opportunity is open to all who share our vision.
+            <br />
+            <br />
+            Together, we are shaping ideas into reality.
 
-        </Typography>
+          </Typography>
+        </Box>
       </Box>
     </Box>
   );
